c9c1b748542126572e1d66f095ed0af83b6f8954
Jin <khjkhj1212@hanmail.net>
2021-09-09 23:05:17 +0900

    Member Test

--- JPA/data-jpa/data-jpa/src/main/java/study/datajpa/domain/Member.java
+++ JPA/data-jpa/data-jpa/src/main/java/study/datajpa/domain/Member.java
@@ -1,20 +1,49 @@
 package study.datajpa.domain;
 
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.util.ArrayList;
 
 @Entity
 @Getter
 @Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Member {
 
     @Id
     @GeneratedValue
+    @Column(name = "member_id")
     private Long id;
 
-    private String name;
+    private String username;
+    
+    int age;
+
+    @ManyToOne
+    @JoinColumn(name = "team_id")
+    private Team team;
+
+    public Member(String username){
+        this.username = username;
+    }
+
+    public Member(String username, int age, Team team){
+        this.username = username;
+        this.age = age;
+        if(team != null){
+            changeTeam(team);
+        }
+
+    }
+
+    public void changeTeam(Team team){
+        this.team = team;
+        team.getMembers().add(this);
+    }
+
+
 }
---
+++ JPA/data-jpa/data-jpa/src/test/java/study/datajpa/repository/MemberJpaRepositoryTest.java
@@ -0,0 +1,29 @@
+package study.datajpa.repository;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
+import study.datajpa.domain.Member;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@Transactional
+@Rollback(false)
+class MemberJpaRepositoryTest {
+
+    @Autowired
+    MemberJpaRepository memberJpaRepository;
+
+    @Test
+    public void testMember(){
+        Member member = new Member("memberA");
+        Member savedMember = memberJpaRepository.save(member);
+
+        Member findMember = memberJpaRepository.find(savedMember.getId());
+
+    }
+
+}
