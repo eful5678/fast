5cbac675bd4ec57318e7b8aa8b75182385953371
Jin <khjkhj1212@hanmail.net>
2021-09-15 08:12:41 +0900

    [테스트] @Query Test
    test

--- JPA/data-jpa/data-jpa/src/test/java/study/datajpa/repository/MemberRepositoryTest.java
+++ JPA/data-jpa/data-jpa/src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -8,6 +8,7 @@ import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
 import study.datajpa.domain.Member;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -32,7 +33,74 @@ class MemberRepositoryTest {
         Assertions.assertThat(result.get(0).getUsername()).isEqualTo("AAA");
         Assertions.assertThat(result.get(0).getAge()).isEqualTo(20);
         Assertions.assertThat(result.size()).isEqualTo(1);
-}
+    }
+    
+    @Test
+    public void findByQuery() throws Exception{
+        //given
+        Member member = new Member("HI MAN", 25);
+        memberRepository.save(member);
+
+        List<Member> result = memberRepository.findUser("HI MAN", 25);
+
+        Assertions.assertThat(result.get(0).getUsername()).isEqualTo("HI MAN");
+        Assertions.assertThat(result.get(0).getAge()).isEqualTo(25);
+        //when
+    }
+
+    @Test
+    public void QueryNameList() throws Exception{
+        //given
+        Member m1 = new Member("Hyun Jin");
+        Member m2 = new Member("Kim");
+
+        memberRepository.save(m1);
+        memberRepository.save(m2);
+
+        List<String> userNameList = memberRepository.findUserNameList();
+        //when
+
+        Assertions.assertThat(userNameList.get(0)).isEqualTo("Hyun Jin");
+        Assertions.assertThat(userNameList.get(1)).isEqualTo("Kim");
+    }
+    
+    @Test
+    public void QueryDto() throws Exception{
+        //given
+
+        //when
+    }
+    
+    @Test
+    public void QueryParameter() throws Exception{
+        //given
+        Member m = new Member("HJ", 32);
+        memberRepository.save(m);
+
+        Member members = memberRepository.findMembers("HJ");
+        //when
+        Assertions.assertThat(members.getUsername()).isEqualTo("HJ");
+    }
+    
+    @Test
+    public void QueryCollection() throws Exception{
+        //given
+        Member m1 = new Member("KHJ", 22);
+        Member m2 = new Member("Kim", 24);
+        memberRepository.save(m1);
+        memberRepository.save(m2);
+
+        List<String> names = new ArrayList<>();
+        names.add("KHJ");
+        names.add("Kim");
+
+        List<Member> results = memberRepository.findNames(names);
+
+        //when
+        Assertions.assertThat(results.get(0)).isEqualTo("KHJ");
+        Assertions.assertThat(results.get(1)).isEqualTo("Kim");
+
+    }
 
 
 
