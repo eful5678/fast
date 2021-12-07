fbd84fcea5f7884bd3f321f76d27a6af853fb71b
Jinh <eful5678@gmail.com>
2021-12-07 20:24:29 +0900

    [VueJS] emit event

---
+++ Vue/vueschool/vueschool-forum/src/components/PostEditor.vue
@@ -0,0 +1,54 @@
+<template>
+  <form @submit.prevent="save">
+    <div class="form-group">
+        <textarea
+          class="form-input"
+          v-model="text"></textarea>
+    </div>
+    <div class="form-actions">
+      <button class="btn-blue">Submit</button>
+    </div>
+  </form>
+</template>
+
+<script>
+export default {
+  name: 'PostEditor',
+  props: {
+    threadId: {
+      required: true,
+      type: String
+    }
+  },
+  data () {
+    return {
+      text: ''
+    }
+  },
+  methods: {
+    save () {
+      const postId = 'greatPost' + Math.random()
+      const post = {
+        publishedAt: Math.floor(Date.now() / 1000),
+        text: this.text,
+        userId: 'ALXhxjwgY9PinwNGHpfai6OWyDu2',
+        threadId: this.threadId,
+        '.key': postId
+      }
+
+      // this.$set(sourceData.posts, postId, post)
+      // this.$set(this.thread.posts, postId, postId)
+
+      // sourceData.posts[postId] = post
+      // this.thread.posts[postId] = postId
+      this.text = ''
+
+      this.$emit('save', {post})
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
--- Vue/vueschool/vueschool-forum/src/pages/PageThreadShow.vue
+++ Vue/vueschool/vueschool-forum/src/pages/PageThreadShow.vue
@@ -1,32 +1,21 @@
 <template>
   <div class="col-large push-top">
     <h2>{{ thread.title }}</h2>
-    <div class="post-list">
-      <div v-for="postId in thread.posts" class="post">
-        <div class="user-info">
-          <a href="#" class="user-name">{{ users[posts[postId].userId].name }}</a>
-          <a href="#">
-            <img class="avatar-large" :src="users[posts[postId].userId].avatar">
-          </a>
-        </div>
-        <div class="post-content">
-          <div>{{ posts[postId].text }}</div>
-        </div>
-        <div class="post-date text-faded">
-          {{ posts[postId].publishedAt }}
-        </div>
-
-      </div>
-    </div>
-
+    <PostList :posts="posts"></PostList>
+    <PostEditor
+      @save="addPost"
+      :threadId="id"></PostEditor>
   </div>
 </template>
 
 <script>
 import sourceData from '@/data'
+import PostList from '../components/PostList'
+import PostEditor from '../components/PostEditor'
 
 export default {
   name: 'PageThreadShow',
+  components: {PostEditor, PostList},
   // to passing down 'id' property to child component
   props: {
     id: {
@@ -37,8 +26,25 @@ export default {
   data () {
     return {
       thread: sourceData.threads[this.id],
-      posts: sourceData.posts,
-      users: sourceData.users
+      newPostText: ''
+    }
+  },
+  computed: {
+    posts () {
+      const postIds = Object.values((this.thread.posts))
+      return Object.values(sourceData.posts)
+        .filter(post => postIds.includes(post['.key']))
+    }
+  },
+  methods: {
+    addPost (eventData) {
+      console.log(eventData)
+      const post = eventData.post
+      const postId = eventData.post['.key']
+
+      this.$set(sourceData.posts, postId, post)
+      this.$set(this.thread.posts, postId, postId)
+      // eventData.post.threadId = this.id
     }
   }
 }
